9f0f7d6b541661c559110368853b1a7121e0426c
Matthew Gips <msg558@gmail.com>
2023-01-24 14:27:06 -0600

    another commit

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -9,7 +9,7 @@ export const Photo = (props: propsType) => {
     return(
         <div>
             <div className='Photo'> 
-                
+                <div></div>
                 <img src={'Photos/'+props.path+'.jpg'} alt='alternate text' className='img'></img>
             </div>
         </div>
